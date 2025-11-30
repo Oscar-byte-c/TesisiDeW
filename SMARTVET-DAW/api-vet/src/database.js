b4a8ef451ee19a20f5877dee045bd99bd9775a3d
xOscar <123287270+Oscar-byte-c@users.noreply.github.com>
2025-11-30 01:34:09 -0500

    Update database.js

--- SMARTVET-DAW/api-vet/src/database.js
+++ SMARTVET-DAW/api-vet/src/database.js
@@ -1,19 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-mongoose.set('strictQuery', true)
+dotenv.config();
 
-const connection = async() => {
+const dbURI = process.env.MONGODB_URI_PRODUCTION; // <- Atlas
 
-    try {
-
-        const {connection} = await mongoose.connect(process.env.MONGODB_URI_LOCAL)
-        console.log(`Database is connected on ${connection.host} - ${connection.port}`)
-
-    } catch(error) {
-        console.log(error);
-    }
-
-}
-
-
-export default connection;
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(dbURI);
+    console.log("✅ Conectado a MongoDB Atlas");
+  } catch (error) {
+    console.error("❌ Error conectando a MongoDB:", error.message);
+    process.exit(1);
+  }
+};
