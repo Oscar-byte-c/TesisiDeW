f915e8f1a0a264f38186e4126f68849a5ac28450
xOscar <123287270+Oscar-byte-c@users.noreply.github.com>
2025-11-30 01:34:39 -0500

    Remove database connection call from index.js

--- SMARTVET-DAW/api-vet/src/index.js
+++ SMARTVET-DAW/api-vet/src/index.js
@@ -1,7 +1,5 @@
 import app from './server.js';
-import connection from './database.js';
 
-connection();
 
 app.listen(app.get('port'), ()=> {
     console.log(`server ok on http://localhost:${app.get('port')}`)
