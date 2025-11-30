7207e19b6fcf4bd01f45d1c11edd89d92ca9af56
xOscar <123287270+Oscar-byte-c@users.noreply.github.com>
2025-11-30 01:33:54 -0500

    Refactor routes to include estudiante functionality

--- SMARTVET-DAW/api-vet/src/routers/veterinario-routes.js
+++ SMARTVET-DAW/api-vet/src/routers/veterinario-routes.js
@@ -1,13 +1,12 @@
 import {Router} from 'express'
-import { confirmarMail,recuperarPassword,comprobarTokenPassword,crearNuevoPassword,registro, login, perfil,actualizarPassword,actualizarPerfil} from '../controllers/veterinario-controller.js';
+import { confirmarMail,recuperarPassword,comprobarTokenPassword,crearNuevoPassword,registro, login,perfil,actualizarPassword,actualizarPerfil,getDesktop,createItem  } from '../controllers/estudiante-controller.js';
 import { verificarTokenJWT } from '../middlewares/JWT.js';
-
 
 const router = Router()
 
 
-console.log("✅ [veterinario-routes.js] Archivo cargado y definiendo rutas.");
-//Publicas
+console.log("✅ [estudiante-routes.js] Archivo cargado y definiendo rutas.");
+
 router.post('/registro', registro);
 
 router.get('/confirmar/:token',confirmarMail)
@@ -19,15 +18,20 @@ router.get('/recuperarPassword/:token',comprobarTokenPassword)
 router.post('/nuevoPassword/:token',crearNuevoPassword)
 
 
-//Privadas
-
-router.post('/veterinario/login',login)
 
 
-router.get('/veterinario/perfil',verificarTokenJWT,perfil)
+router.post('/estudiante/login',login)
 
-router.put('/veterinario/perfil/:id',verificarTokenJWT,actualizarPerfil)
+router.get('/estudiante/perfil',verificarTokenJWT,perfil)
+
+router.put('/estudiante/perfil/:id',verificarTokenJWT,actualizarPerfil)
 
 router.put('/actualizarPassword/:id',verificarTokenJWT,actualizarPassword)
+
+// SB-B002 – Obtener ítems del escritorio
+router.get("/desktop", verificarTokenJWT, getDesktop);
+
+// SB-B003 – Crear un nuevo ítem
+router.post("/items", verificarTokenJWT, createItem);
 
 export default router;
